baf757679439c5790e9888a5a21abb8772a88bc6
utkarsh-raj <u.rajsingh2503@gmail.com>
2018-07-20 16:49:56 +0530

    Application core required files

---
+++ script.js
@@ -0,0 +1,36 @@
+var quote = document.querySelector("#quote");
+var author = document.querySelector("#author");
+var button = document.querySelector("button");
+
+
+
+
+
+
+quote.innerHTML = "Hello from div!";
+
+
+var quotes = [
+		{
+			author: "Jai Dewani",
+			quote: "Hello this is a quote!"
+		},
+		{
+			author: "Jai Dewani",
+			quote: "Hello this is another quote!"
+		},
+		{
+			author: "Jai Dewani",
+			quote: "Hello this is also a quote!"
+		},
+		{
+			author: "Jai Dewani",
+			quote: "Hello this is a quote by Jai!"
+		}
+	]
+
+button.addEventListener("click", function() {
+	newQuote = quotes[Math.floor(Math.random() * quotes.length)];
+	quote.innerHTML = newQuote["quote"];
+	author.innerHTML = newQuote["author"];
+});
